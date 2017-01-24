22523f72a09fac29c689aebcef7d66bd8a36ae63
Bence Kiglics <bence.kiglics@gmail.com>
2017-01-24 08:06:42 +0100

    Downloading all packages

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM ubuntu:xenial
 
-RUN apt-get update && apt-get install -y less man manpages-dev
+ENV DEBIAN_FRONTEND=noninteractive TERM=xterm
+RUN apt-get update && apt-get install -y less man manpages-dev binutils xz-utils
 
 COPY entrypoint.sh /entrypoint
 COPY less_termcap /etc/less_termcap
+
+COPY scripts/download_manuals.sh /opt/download_manuals.sh
+RUN /opt/download_manuals.sh
 
 ENTRYPOINT ["/entrypoint"]
 CMD ["man"]
---
+++ scripts/download_manuals.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+function main() {
+    local packages=$(list_packages)
+    local count=$(echo "$packages" | wc -l)
+    local current=0
+
+    while read package; do
+        printf "[%3d%%] " $(($current * 100 / $count))
+        download_manual_for_package "$package"
+        current=$(($current+1))
+    done <<< "$packages"
+
+    rebuild_manual_db
+}
+
+function list_packages() {
+    apt-cache dumpavail | awk '/^Package: / {print $2}' | head -n 25
+}
+
+function download_manual_for_package() {
+    local pkg=$1
+    local tmpdir
+
+    (
+        echo -n "Installing man pages for '$pkg'... "
+        tmpdir=$(mktemp -d)
+        chmod 777 "$tmpdir"
+        cd "$tmpdir"
+
+        apt-get download "$pkg" >/dev/null
+        echo -n 'downloaded '
+
+        ar x *.deb
+        tar xf data.tar.*
+        echo -n 'extracted '
+
+        if [[ -d usr/share/man ]]; then
+            cp -rp usr/share/man/man* /usr/share/man
+            echo -n 'copied'
+        else
+            echo -n 'skipped'
+        fi
+
+        rm -rf $tmpdir
+        echo
+    )
+}
+
+function rebuild_manual_db() {
+    mandb
+}
+
+main
